4a61c317d5dc2c0bbd6e726d4a8bd365ddb393d8
Kirev_D <Kirev_D@utkonos.ru>
2021-03-17 15:58:26 +0200

    feat: 'add news api and filter posts'

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,14 @@
-import Nav from './Nav';
+import Nav from "./Nav";
 
 const Layout = ({ mainTitle, footer, children }) => (
     <div>
         <Nav />
         <h1>{mainTitle}</h1>
+        <hr />
         {children}
         <hr />
-        {footer}
+        <h4>{footer}</h4>
     </div>
-)
+);
 
 export default Layout;
--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,35 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import Layout from "../coponents/Layout";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
+const myStyle = {
+    color: "red",
+    borderLeft: "5px solid black",
+    padding: "5px"
+};
 
 const About = () => (
     <Layout
-        mainTitle={'My about build with Next JS'}
-        footer={`Copyright ${new Date().getFullYear()}`}
+        mainTitle="My About Page built with Next JS"
+        footer={`This is built by Ryan`}
     >
-        <Head>
-            <title>About page</title>
-        </Head>
         <h2>About page</h2>
-        <Link href="/">Home page</Link>
+        <Link href="/">
+            <a style={myStyle}>Home page</a>
+        </Link>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+            odio, fuga error, cum, veniam excepturi quos nulla similique
+            explicabo libero corrupti nobis minus eaque eligendi voluptatibus
+            sequi nesciunt aliquid eius!
+        </p>
+
+        <style jsx>{`
+            p {
+                color: indigo;
+                font-size: 20px;
+            }
+        `}</style>
     </Layout>
 );
 
